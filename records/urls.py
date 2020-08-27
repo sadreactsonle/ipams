7cc402bbc4316e227be595f948b5d6901b0181cc
anounymous <mranonymous2439@gmail.com>
2020-08-27 13:23:34 +0800

    changes

--- records/urls.py
+++ records/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('uploadexcel/', views.ParseExcel.as_view(), name='records-upload'),
     path('downloadformat/', views.download_format, name='records-download-format'),
     path('download/abstract/<int:record_id>', views.download_abstract, name='records-download-abstract'),
-    path('records/user', views.MyRecordsView.as_view(), name='records-myrecords'),
-    path('records/pending', views.PendingRecordsView.as_view(), name='records-pending'),
-    path('records/approved', views.ApprovedRecordsView.as_view(), name='records-approved'),
-    path('records/declined', views.DeclinedRecordsView.as_view(), name='records-declined'),
+    path('user/', views.MyRecordsView.as_view(), name='records-myrecords'),
+    path('pending/', views.PendingRecordsView.as_view(), name='records-pending'),
+    path('approved/', views.ApprovedRecordsView.as_view(), name='records-approved'),
+    path('declined/', views.DeclinedRecordsView.as_view(), name='records-declined'),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
